27a77a2e9f787565485fe9fddbd4066f394fee84
ptishkevich <ptishkevich@neoflex.ru>
2023-06-08 15:10:12 +0400

    Initial commit

---
+++ src/main/java/com/example/neohackproject/NeoHackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.neohackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoHackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoHackProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neohackproject/NeoHackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neohackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoHackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
